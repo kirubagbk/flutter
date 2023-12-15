a2db4d16d9d18e4c9f3b072a1a304a69f1de3338
kirubagbk <87473471+kirubagbk@users.noreply.github.com>
2023-12-15 16:42:36 +0530

    Add files via upload

---
+++ splash.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class SplashScreen extends StatelessWidget{
+  const SplashScreen({super.key});
+  @override
+ Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter chat'),
+        
+      ),
+      body: const Center(child: Text(' Loading...'),
+      ),
+    );
+  }
+  
+  
+  }
